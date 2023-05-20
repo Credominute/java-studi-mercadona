fd3f8cf3ce4518f3860250cb4a8fc5e902c898df
Jean-Yves Rosaye <83571685+Credominute@users.noreply.github.com>
2023-05-20 16:43:45 +0200

    routing actualisation finale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: []
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
