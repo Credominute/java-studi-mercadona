f9b798a6bd4339c633fc22b5abfb08dfd8c4e0ff
Jean-Yves Rosaye <83571685+Credominute@users.noreply.github.com>
2023-06-22 11:07:02 +0200

    Configuration de Spring security avec API swagger

--- src/main/java/fr/studi/promoweb/security/configuration/JwtAuthenticationFilter.java
+++ src/main/java/fr/studi/promoweb/security/configuration/JwtAuthenticationFilter.java
@@ -17,19 +17,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtService jwtService;
-
     @Autowired
     private UserDetailsService userDetailsService;
-
     @Autowired
     private TokenRepository tokenRepository;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
@@ -38,32 +36,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
+        if(request.getServletPath().contains("/swagger-ui/**")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
         String jwt= "";
         String userEmail = "";
 
-        if(!authHeader.startsWith("Bearer ")){
+        if(authHeader != null && !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
         }
 
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
         if(userEmail != null &&
                 SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             boolean isTokenValid = tokenRepository.findByToken(jwt)
                     .map(t -> !t.isExpired() && !t.isRevoked())
                     .orElse(false);
-
             if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
-
         filterChain.doFilter(request,response);
-
     }
 }
--- src/main/java/fr/studi/promoweb/security/configuration/SecurityConfig.java
+++ src/main/java/fr/studi/promoweb/security/configuration/SecurityConfig.java
@@ -3,10 +3,12 @@ package fr.studi.promoweb.security.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,32 +20,51 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
-
+    static final String[] AUTH_WHITE_LIST = {
+            "/api/auth",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v2/api-docs/**",
+            "/swagger-resources/**"
+    };
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
     private final LogoutHandler logoutHandler;
-
     private final AuthenticationProvider authenticationProvider;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf()
-                .disable()
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                .requestMatchers(AUTH_WHITE_LIST).permitAll()
+                //.requestMatchers("/api/**").authenticated()
+                .anyRequest().authenticated()
+                //.anyRequest().permitAll()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
+
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
                 .logoutUrl("/api/logout")
                 .addLogoutHandler(logoutHandler)
                 .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext()));
         return httpSecurity.build();
+              /*  .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/v2/api-docs").permitAll()
+                .pathMatchers("/configuration/ui").permitAll()
+                .pathMatchers("/swagger-resources/**").permitAll()
+                .pathMatchers("/configuration/security").permitAll()
+                .pathMatchers("/swagger-ui.html").permitAll()
+                .pathMatchers("/swagger-ui/*").permitAll()
+                .pathMatchers("/webjars/**").permitAll()
+                .pathMatchers("/v2/**").permitAll()
+                .and().cors()
+                .and().oauth2ResourceServer()
+                .jwt().and().and()
+        return httpSecurity.build();*/
     }
 }
